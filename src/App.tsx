2fdfdac3120fd47a86f6f0e95847cba7a4e4c21b
Lionel COTE <grcote7@gmail.com>
2023-06-08 15:42:59 +0200

    Fix: add Key ID for TS

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,8 @@ import "./styles.css";
 
 function App() {
   // State (État, données)
-  const [fruits, setFruits] = useState([
+  // const [fruits, setFruits] = useState([
+  const [fruits] = useState([
     { id: 1, nom: "Abricot" },
     { id: 2, nom: "Banane" },
     { id: 3, nom: "Cerise" }
@@ -15,12 +16,12 @@ function App() {
   // Render (Affichage)
   return (
     <div className="container">
-      
+
       <h1>Liste de fruits</h1>
 
       <ul>
         {fruits.map((fruit) => (
-          <li>
+          <li key={fruit.id}>
             {fruit.id} - {fruit.nom} <button>X</button>
           </li>
         ))}
