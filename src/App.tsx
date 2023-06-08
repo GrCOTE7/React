04bdb6a9ca667b8e737772af9ac2b9225d110cde
Lionel COTE <grcote7@gmail.com>
2023-06-08 16:14:29 +0200

    Feat: Delete Item in a list

--- src/App.tsx
+++ src/App.tsx
@@ -3,30 +3,41 @@ import "./styles.css";
 
 function App() {
   // State (État, données)
-  // const [fruits, setFruits] = useState([
-  const [fruits] = useState([
+  const [fruits, setFruits] = useState([
     { id: 1, nom: "Abricot" },
     { id: 2, nom: "Banane" },
     { id: 3, nom: "Cerise" }
   ]);
 
   // Comporments (Comportements, évènements)
+  const handleDelete = (id) => {
+    console.log("handleDelete", id);
 
+    // 1. Copie du State
+    // const fruitsCopy = fruits.slice(); // 1st way
+    const fruitsCopy = [...fruits]; // 2nd way - More frequently seen
+    console.log(fruitsCopy);
+
+    // 2. Manipuler la cpie du State
+    const fruitsCopyUpdated = fruitsCopy.filter((fruit) => fruit.id !== id);
+    console.log("fruitsCopyUpdated", fruitsCopyUpdated);
+
+    // 3. Modifier le state avec le setter
+    setFruits(fruitsCopyUpdated);
+  };
 
   // Render (Affichage)
   return (
-    <div className="container">
-
+    <div>
       <h1>Liste de fruits</h1>
-
       <ul>
         {fruits.map((fruit) => (
           <li key={fruit.id}>
-            {fruit.id} - {fruit.nom} <button>X</button>
+            {fruit.id} - {fruit.nom}{" "}
+            <button onClick={() => handleDelete(fruit.id)}>X</button>
           </li>
         ))}
       </ul>
-
     </div>
   );
 }
