59f2ddb0d527a13c82f5a30ea8affb9c4ecf0cea
Lionel COTE <grcote7@gmail.com>
2023-06-12 11:28:56 +0200

    Fix: allowImportingTsExtensions

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.tsx';
-import Compteur from './Compteur.tsx';
 
-
-
-// <App />,
 ReactDOM.render(
-  <Compteur />,
+  <App />,
   document.getElementById('root')
 );    
