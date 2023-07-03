1c75ee739a4645a7219e52563d475c17e519ccf7
Lionel COTE <grcote7@gmail.com>
2023-07-03 15:46:03 +0200

    Feat: Add colored label

--- src/components/pokemon-card.tsx
+++ src/components/pokemon-card.tsx
@@ -27,6 +27,51 @@ const PokemonCard: FunctionComponent<Props> = ({ pokemon, borderColor = '#009688
     return `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`
   }
 
+  const formatType = (type: string): string => {
+    let color: string;
+
+    switch (type) {
+      case 'Feu':
+        color = 'red lighten-1';
+        break;
+      case 'Eau':
+        color = 'blue lighten-1';
+        break;
+      case 'Plante':
+        color = 'green lighten-1';
+        break;
+      case 'Insecte':
+        color = 'brown lighten-1';
+        break;
+      case 'Normal':
+        color = 'grey lighten-3';
+        break;
+      case 'Vol':
+        color = 'blue lighten-3';
+        break;
+      case 'Poison':
+        color = 'deep-purple accent-1';
+        break;
+      case 'Fée':
+        color = 'pink lighten-4';
+        break;
+      case 'Psy':
+        color = 'deep-purple darken-2';
+        break;
+      case 'Electrik':
+        color = 'lime accent-1';
+        break;
+      case 'Combat':
+        color = 'deep-orange';
+        break;
+      default:
+        color = 'grey';
+        break;
+    }
+
+    return `chip ${color}`;
+  }
+
   return (
     <div className="col 
             m6 l4"
@@ -48,7 +93,7 @@ const PokemonCard: FunctionComponent<Props> = ({ pokemon, borderColor = '#009688
             <p>{formatDate(pokemon.created)}</p>
 
             <ul>
-              {pokemon.types.map(type => (<li key={type}>{type}</li>))}
+              {pokemon.types.map(type => (<li key={type} className={formatType(type)}>{type}</li>))}
             </ul>
 
           </div>
